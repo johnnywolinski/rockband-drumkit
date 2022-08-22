f608191d4e54c944d7382b7a43b3af6e4237fdc5
johnnywolinski <101757344+johnnywolinski@users.noreply.github.com>
2022-08-21 22:39:18 -0500

    A
    A

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-const gameBoard = document.querySelector('.gameBoard');
-let rows = Array.from(document.querySelectorAll('.gameBoard div'));
-note = [0, 1, 2]
-
-function createNote(){
-  console.log(note.isArray)
-}
-
-createNote()
-})
+const snare = document.querySelector('snare');
+const bass = document.getElementById('bass');
+const tom1 = document.getElementById('tom1');
+const tom2 = document.getElementById('tom2');
+const cymb = document.getElementById('cymb');
+function playSound(event){
+  if(event.keyCode === 65 )
+  {const snareSound = new Audio('snare.mp3');
+    snareSound.play();}
+  if(event.keyCode === 83 )
+  {const bassSound = new Audio('bass.mp3');
+    bassSound.play()}
+  if(event.keyCode === 68 )
+  {const tom1Sound = new Audio('tom1.mp3');
+    tom1Sound.play()}
+  if(event.keyCode === 70 )
+  {const tom2Sound = new Audio('tom2.mp3');
+    tom2Sound.play()
+    console.log(typeof(tom2Sound))}
+  if(event.keyCode === 71 )
+  {const cymbalSound = new Audio('hihat.mp3');
+    cymbalSound.play();
+  console.log(typeof(cymbalSound))
+}}
+document.addEventListener('keydown', playSound);
