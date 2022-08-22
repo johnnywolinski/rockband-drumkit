93ca1d23bd9ec28017dff1f10152c3536e438e71
johnnywolinski <101757344+johnnywolinski@users.noreply.github.com>
2022-08-21 22:34:22 -0500

    s
    s

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+const gameBoard = document.querySelector('.gameBoard');
+let rows = Array.from(document.querySelectorAll('.gameBoard div'));
+note = [0, 1, 2]
+
+function createNote(){
+  console.log(note.isArray)
+}
+
+createNote()
+})
